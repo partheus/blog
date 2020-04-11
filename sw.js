985c7131146dd6c6f2229a38d9ac7abf98dea5b4
Parth <parth.shiralkar@gmail.com>
2020-04-11 18:20:35 -0500

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-834cd26a51414e160c1e.js"
+    "url": "webpack-runtime-0df86ab51d241783e214.js"
   },
   {
     "url": "styles.e0ae556c92d231f4abbd.css"
@@ -42,7 +42,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "4bf4199ccbe567dcdaf1e351991591fc"
+    "revision": "2183fa847a197296c91bb2835a149bc8"
   },
   {
     "url": "component---src-pages-404-js-3b38fcfe6e80e76364d0.js"
